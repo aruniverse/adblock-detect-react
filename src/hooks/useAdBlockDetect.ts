d6064a1a369fbaf86048a6a824565932f20cbcfc
Arun George <aruniverse@users.noreply.github.com>
2020-07-29 20:25:26 -0400

    initial comit. moving machines

---
+++ src/hooks/useAdBlockDetect.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export const useAdBlockDetect = () => {
+  const [adBlockEnabled, setAdblockEnabled] = useState(false);
+
+  useEffect(() => {
+    const adCheck = document.createElement("div");
+    adCheck.innerHTML = "&nbsp;";
+    adCheck.className = "adsbox";
+    document.body.appendChild(adCheck);
+
+    if (adCheck?.offsetHeight === 0) {
+      setAdblockEnabled(true);
+    }
+
+    return adCheck.remove();
+  }, []);
+
+  return adBlockEnabled;
+};
