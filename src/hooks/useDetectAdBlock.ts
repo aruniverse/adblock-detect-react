d9d30d2a299657064f292d3e27c72fac274fd018
Arun George <aruniverse@users.noreply.github.com>
2020-08-02 23:19:20 -0400

    update how ads are checked to be more thorough. update minor things throughout repo

--- src/hooks/useDetectAdBlock.ts
+++ src/hooks/useDetectAdBlock.ts
@@ -5,15 +5,41 @@ export const useDetectAdBlock = () => {
 
   useEffect(() => {
     const adToDetect = document.createElement("div");
-    adToDetect.innerHTML = "&nbsp;";
-    adToDetect.className = "adsbox";
+    adToDetect.setAttribute(
+      "class",
+      "pub_300x250 pub_300x250m pub_728x90 text-ad textAd text_ad text_ads text-ads text-ad-links"
+    );
+    adToDetect.setAttribute(
+      "style",
+      "width: 1px ! important; height: 1px !important; position: absolute !important; left: -10000px !important; top: -1000px !important;"
+    );
     document.body.appendChild(adToDetect);
 
-    if (adToDetect?.offsetHeight === 0) {
+    if (
+      window.document.body.getAttribute("abp") !== null ||
+      adToDetect.offsetParent === null ||
+      adToDetect.offsetHeight == 0 ||
+      adToDetect.offsetLeft == 0 ||
+      adToDetect.offsetTop == 0 ||
+      adToDetect.offsetWidth == 0 ||
+      adToDetect.clientHeight == 0 ||
+      adToDetect.clientWidth == 0
+    ) {
       setAdBlockDetected(true);
+    } else if (window.getComputedStyle !== undefined) {
+      const adToDetectTemp = window.getComputedStyle(adToDetect, null);
+      if (
+        adToDetectTemp &&
+        (adToDetectTemp.getPropertyValue("display") == "none" ||
+          adToDetectTemp.getPropertyValue("visibility") == "hidden")
+      ) {
+        setAdBlockDetected(true);
+      }
     }
 
-    return adToDetect.remove();
+    return () => {
+      document.body.removeChild(adToDetect);
+    };
   }, []);
 
   return adBlockDetected;
