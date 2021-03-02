72f3b5852e687ab746938ec939d09c5ca71a8a95
Arun George <aruniverse@users.noreply.github.com>
2021-03-01 23:36:59 -0500

    update honeypot

--- adblock-detect-react/src/hooks/useDetectAdBlock.ts
+++ adblock-detect-react/src/hooks/useDetectAdBlock.ts
@@ -4,55 +4,24 @@ export const useDetectAdBlock = () => {
   const [adBlockDetected, setAdBlockDetected] = useState(false);
 
   useEffect(() => {
-    const adToDetect = document.createElement("div");
-    adToDetect.setAttribute(
-      "class",
-      "googlead pub_300x250 pub_300x250m pub_728x90 text-ad textAd text_ad text_ads text-ads text-ad-links"
-    );
-    adToDetect.setAttribute(
-      "style",
-      "width: 1px ! important; height: 1px !important; position: absolute !important; left: -10000px !important; top: -1000px !important;"
-    );
-    document.body.appendChild(adToDetect);
-
-    if (
-      window.document.body.getAttribute("abp") !== null ||
-      adToDetect.offsetParent === null ||
-      adToDetect.offsetHeight == 0 ||
-      adToDetect.offsetLeft == 0 ||
-      adToDetect.offsetTop == 0 ||
-      adToDetect.offsetWidth == 0 ||
-      adToDetect.clientHeight == 0 ||
-      adToDetect.clientWidth == 0
-    ) {
-      setAdBlockDetected(true);
-    } else if (window.getComputedStyle !== undefined) {
-      const adToDetectTemp = window.getComputedStyle(adToDetect, null);
-      if (
-        adToDetectTemp &&
-        (adToDetectTemp.getPropertyValue("display") == "none" ||
-          adToDetectTemp.getPropertyValue("visibility") == "hidden")
-      ) {
-        setAdBlockDetected(true);
-      }
-    }
-
-    return () => {
-      document.body.removeChild(adToDetect);
-    };
-  }, []);
-
-  useEffect(() => {
-    // fail safe to double check
-    if (!adBlockDetected) {
-      fetch("pagead/js/adsbygoogle.js", {
-        method: "HEAD",
-        mode: "no-cors",
-        cache: "no-store",
-      }).catch(() => {
+    // grab a domain from https://github1s.com/gorhill/uBlock/blob/master/docs/tests/hostname-pool.js
+    const url = "https://www3.doubleclick.net";
+    fetch(url, {
+      method: "HEAD",
+      mode: "no-cors",
+      cache: "no-store",
+    })
+      .then((res) => {
+        if (process.env.NODE_ENV !== "production") {
+          console.log(res);
+        }
+      })
+      .catch((err) => {
+        if (process.env.NODE_ENV !== "production") {
+          console.log(err);
+        }
         setAdBlockDetected(true);
       });
-    }
   }, []);
 
   return adBlockDetected;
