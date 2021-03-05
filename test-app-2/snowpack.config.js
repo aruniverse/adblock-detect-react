bd992afd2e2d0f0010a0940fc5c3a8fa63e5ecc2
Arun George <aruniverse@users.noreply.github.com>
2021-03-05 15:54:41 -0500

    create new snowpack app

---
+++ test-app-2/snowpack.config.js
@@ -0,0 +1,27 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-typescript'],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    bundle: true,
+    treeshake: true,
+    minify: true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    port: 3000,
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
