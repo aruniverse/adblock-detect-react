a5033c417b30f3ceeb226d81e7321cc61e9548ed
Arun George <11051042+aruniverse@users.noreply.github.com>
2021-12-22 15:29:23 -0500

    switch to pnpm + turbo (#17)
    Co-authored-by: Arun George <aruniverse@users.noreply.github.com>

--- vite-test/src/App.jsx
+++ vite-test/src/App.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect } from 'react';
-import ReactGA from 'react-ga';
+import { useEffect } from "react";
+import ReactGA from "react-ga";
+import logo from "./logo.svg";
+import "./App.css";
 
-import logo from './logo.svg';
-import './App.css';
-
-import { useDetectAdBlock } from 'adblock-detect-react';
-import { detect } from 'detect-browser';
+import { useDetectAdBlock } from "adblock-detect-react";
+import { detect } from "detect-browser";
 
 function App() {
   useEffect(() => {
-    const { SNOWPACK_PUBLIC_GOOGLE_TRACKING_ID } = import.meta.env;
-    ReactGA.initialize(SNOWPACK_PUBLIC_GOOGLE_TRACKING_ID);
+    const { VITE_GOOGLE_TRACKING_ID } = import.meta.env;
+    ReactGA.initialize(VITE_GOOGLE_TRACKING_ID);
     ReactGA.pageview(window.location.origin);
   }, []);
 
@@ -20,11 +19,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>{JSON.stringify(browser)}</div>
+        <pre>{JSON.stringify(browser, null, 2)}</pre>
         {adBlockDetected && (
           <>
             <img src={logo} className="App-logo" alt="logo" />
-            <div>{'An ad-blocker has been detected!'}</div>
+            <div>{"An ad-blocker has been detected!"}</div>
             <a
               className="App-link"
               href="https://www.npmjs.com/package/adblock-detect-react"
